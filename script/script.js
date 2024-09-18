ac77d2431506d17ab1ac54cd7f0148c1f3daf5d6
Emmanuel George <emmamarine005@gmail.com>
2024-09-18 07:11:20 +0100

    First Commit

--- script/script.js
+++ script/script.js
@@ -4,6 +4,12 @@ const custom = document.querySelector(".custom");
 const birthdayToggle = document.querySelector(".birthday-cake");
 const weddingToggle = document.querySelector(".wedding-cake");
 const customToggle = document.querySelector(".custom-cake");
+const navToggle = document.querySelector(".nav-toggle");
+const menu = document.querySelector(".mobile-links");
+const dropDownToggle = document.querySelectorAll(".drop-down-toggle");
+const dropDown = document.querySelectorAll(".drop-down");
+const navEl = document.querySelectorAll("nav a");
+const ScrollTop = document.querySelector(".back-to-top");
 
 // ================================ CHANGE ACTIVE CAKE TYPE ==================================
 birthdayToggle.addEventListener("click", add);
@@ -42,3 +48,40 @@ function removeActiveClasses() {
     sort.classList.remove("active");
   });
 }
+
+// ================================== TOGGLE NAVBar functions ===================================
+
+navToggle.addEventListener("click", () => {
+  menu.classList.toggle("active");
+});
+dropDownToggle.forEach((e) => {
+  e.onclick = () => {
+    dropDown.forEach((e) => {
+      e.classList.toggle("active");
+    });
+  };
+});
+navEl.forEach((e) => {
+  e.onclick = () => {
+    removeClasses();
+    e.classList.add("active");
+  };
+});
+
+function removeClasses() {
+  navEl.forEach((e) => {
+    e.classList.remove("active");
+    menu.classList.remove("active");
+    dropDown.forEach((e) => {
+      e.classList.remove("active");
+    });
+  });
+}
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= 80) {
+    ScrollTop.style.display = "grid";
+  } else {
+    ScrollTop.style.display = "none";
+  }
+});
